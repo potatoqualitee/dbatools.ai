29841350a85bbd56b60288ed586728f2dcfab6a5
Chrissy LeMaire <clemaire@gmail.com>
2024-10-01 18:02:22 +0200

    Update setup.ps1

--- .devcontainer/scripts/setup.ps1
+++ .devcontainer/scripts/setup.ps1
@@ -1,7 +1,10 @@
 # psreadline loses its mind trying to save
 Set-PSReadLineOption -HistorySaveStyle SaveNothing
+
+# do it
 Write-Output "Installing PowerShell dependencies..."
 $PSDefaultParameterValues["*:Confirm"] = $false
 $PSDefaultParameterValues["*:Force"] = $true
+Install-PackageProvider -Name NuGet
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module dbatools, psopenai
