a9fd8e869c9fcc37240de2f1cdfd0a46dd8ef549
Chrissy LeMaire <clemaire@gmail.com>
2024-10-01 11:39:25 +0200

    WORKS

--- .devcontainer/scripts/setup.ps1
+++ .devcontainer/scripts/setup.ps1
@@ -3,4 +3,3 @@ $PSDefaultParameterValues["*:Confirm"] = $false
 $PSDefaultParameterValues["*:Force"] = $true
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module dbatools, psopenai
-Import-Module dbatools, psopenai
