d8ad9490512c8052b8e4d7ffbdfef4bc9c252759
Chrissy LeMaire <clemaire@gmail.com>
2024-10-01 10:50:33 +0200

    Update setup.ps1

--- .devcontainer/scripts/setup.ps1
+++ .devcontainer/scripts/setup.ps1
@@ -4,5 +4,3 @@ $PSDefaultParameterValues["*:Force"] = $true
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module dbatools, psopenai
 Import-Module dbatools, psopenai
-Write-Output "Starting SQL Server..."
-Start-Job -Name mssql -ScriptBlock { /opt/mssql/bin/sqlservr }
