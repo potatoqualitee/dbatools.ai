80f583b503df7fc5d5d72209a28f0d79f2c6c5d9
Chrissy LeMaire <clemaire@gmail.com>
2024-10-01 18:54:09 +0200

    Update setup.ps1

--- .devcontainer/scripts/setup.ps1
+++ .devcontainer/scripts/setup.ps1
@@ -2,7 +2,5 @@
 Write-Output "Installing PowerShell dependencies..."
 $PSDefaultParameterValues["*:Confirm"] = $false
 $PSDefaultParameterValues["*:Force"] = $true
-$PSDefaultParameterValues["*:Scope"] = "CurrentUser"
-Install-PackageProvider -Name NuGet
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module dbatools, psopenai, PSReadLine
