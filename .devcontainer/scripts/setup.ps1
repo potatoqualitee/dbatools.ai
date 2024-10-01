34b6a5fea46f77d1acadf41333bbaea49851e7ef
Chrissy LeMaire <clemaire@gmail.com>
2024-10-01 18:34:31 +0200

    fix it

--- .devcontainer/scripts/setup.ps1
+++ .devcontainer/scripts/setup.ps1
@@ -1,10 +1,8 @@
-# psreadline loses its mind trying to save
-Set-PSReadLineOption -HistorySaveStyle SaveNothing
-
 # do it
 Write-Output "Installing PowerShell dependencies..."
 $PSDefaultParameterValues["*:Confirm"] = $false
 $PSDefaultParameterValues["*:Force"] = $true
+$PSDefaultParameterValues["*:Scope"] = "CurrentUser"
 Install-PackageProvider -Name NuGet
 Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module dbatools, psopenai
+Install-Module dbatools, psopenai, PSReadLine
