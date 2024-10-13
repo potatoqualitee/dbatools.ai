3df98a8370bdbb7472abfad41b02071c3c1867c9
Chrissy LeMaire <clemaire@gmail.com>
2024-10-13 21:38:14 +0200

    aider

--- .devcontainer/scripts/setup.ps1
+++ .devcontainer/scripts/setup.ps1
@@ -2,16 +2,21 @@ Write-Output "Installing PowerShell dependencies..."
 $PSDefaultParameterValues["*:Confirm"] = $false
 $PSDefaultParameterValues["*:Force"] = $true
 Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module dbatools, psopenai
-Install-Module finetuna, aitoolkit, Pester
 
-Write-Output "Setting up Northwind..."
-Write-Output "Downloading Northwind..."
-Invoke-WebRequest https://raw.githubusercontent.com/microsoft/sql-server-samples/refs/heads/master/samples/databases/northwind-pubs/instnwnd.sql -OutFile /home/mssql/instnwnd.sql
+# Check if modules are already installed
+if (-not (Get-Module -ListAvailable -Name dbatools)) {
+    Install-Module dbatools, Pester, aitoolkit
+    Install-Module psopenai
+    Install-Module finetuna
+}
 
-# Reload profile with some settings with need
+# Reload profile with some settings we need
 . $profile
 
-Write-Output "Importing Northwind..."
-Invoke-DbaQuery -SqlInstance localhost -Database master -Query "CREATE DATABASE Northwind"
-Invoke-DbaQuery -SqlInstance localhost -Database Northwind -InputFile /home/mssql/instnwnd.sql
+# Check if the Northwind database already exists
+if (-not (Get-DbaDatabase -SqlInstance localhost -Database Northwind)) {
+    Write-Output "Importing Northwind..."
+    Invoke-WebRequest https://raw.githubusercontent.com/microsoft/sql-server-samples/refs/heads/master/samples/databases/northwind-pubs/instnwnd.sql -OutFile /home/mssql/instnwnd.sql
+    Invoke-DbaQuery -SqlInstance localhost -Database master -Query "CREATE DATABASE Northwind"
+    Invoke-DbaQuery -SqlInstance localhost -Database Northwind -InputFile /home/mssql/instnwnd.sql
+}
